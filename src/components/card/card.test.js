d05d3d00a6fa415211d5f44e9abf9e5bc1d195ac
tw1ttt3r <tw1ttt3r@gmail.com>
2023-12-05 23:11:22 -0600

    se arregla problema de update de ref

--- src/components/card/card.test.js
+++ src/components/card/card.test.js
@@ -1,24 +1,25 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import Card from './Card';
 import movies from '../../data/movies.json';
 
+const { id, name, isbn, price, image } = movies[2];
+const props = { src:image , nombre: name ,isbn, precio:price, alt: `pelicula-${id}`};
+
 describe("Suite de tes del componente Card", () => {
     test("existe la imagen con un alt text pelicula-3", () => {
-        const { id, name, isbn, price, image } = movies[2];
-        const props = { src:image , nombre: name ,isbn, precio:price, alt: `pelicula-${id}`};
         render(<Card {...props} />);
         const resultado = screen.getByAltText(`pelicula-${id}`)
         expect(resultado).toBeInTheDocument();
     })
 
-    test("Que el boton agregar este acumulando correctamente", () => {
-        const { id, name, isbn, price, image } = movies[2];
-        const props = { src:image , nombre: name ,isbn, precio:price, alt: `pelicula-${id}`};
-        render(<Card {...props} />);
+    test("Que el boton agregar este acumulando correctamente", async () => {
+        const {rerender} = render(<Card {...props} />);
         const boton = screen.getByText("Agregar")
         fireEvent.click(boton);
+        rerender(<Card {...props} />);
         fireEvent.click(boton);
+        rerender(<Card {...props} />);
         const texto = screen.getByText("Unidades a comprar: 2")
-        expect(resultado).toBeInTheDocument();
+        expect(texto).toBeInTheDocument()
     })
 })
